bc9b7d2b54307bda45a99c2111507a2827bedb78
Pedro Santos <f.santosPedro225@gmail.com>
2025-04-11 15:09:31 -0300

    Commit do Novo valor de aluguel e inquilinos atualizados.

--- distribuicaoValores.py
+++ distribuicaoValores.py
@@ -1,25 +1,3 @@
-'''
-Bem vindo! Este é meu programa para distribuição de valores, este programa foi criado com um simples intúito: distribuir valores para inquilinos.
-
-#MOTIVO
-Gerencio apartamentos, tomo conta de valores a receber, dados pessoas, contratos, etc. Daí então criei esta aplicação que
-faz calculos e fornece valores para cada pessoa automaticamente; usei RPA para isso (pyautogui) como podem ver.
-
-#FUNCIONAMENTO
-O programa pede 2 entradas: Data de expedição da conta de água e o valor, com esses dados o programa começa a passar nas informações de cada inquilino
-e realiza o calculo baseando-se na quantidade de pessoas que moram no apartamento.
-
-Por exemplo: se tiver 1 adulto morando no apartamento e a conta de água no mês for 700:
-** 700.00 / total de inquilinos que moram no mesmo predio * a quantidade de inquilinos que moram neste apartamento específico (neste caso é 1) **
-A distribuição é feita através do Watssapp.
-
-#OBSERVAÇÕES IMPORTANTES:
-Este programa foi pensado e desenvolvido no meu 3° semestre (2023), assim quando tinha passado pela linguagem C, atualmente estou na linguagem JAVA e em breve irei criar
-outro projeto para com este mesmo intúito. Muito Obrigado pela visualização, abraços!
-
-pedro_henrique
-'''
-
 import pyautogui as p
 
 def pular2():
@@ -27,21 +5,17 @@ def pular2():
     p.hotkey('Shift', 'enter')
 
 def distribuicao(conta):    #geralmente são um casal
-    return (conta / 14) * 2 #14 inquilinos no total
+    return (conta / 16) * 2 #16 inquilinos no total
 
 def pec(nome,conta,distri,leitura,apto,v_mensalid): #Todas as 'diversidades' vem para esta função
 
-    if (nome == ' Samira'): #Samira mora sozinha e em um apartamento menor:
+    if(nome == ' Flavia' or nome == ' Geane' or nome == ' Beatriz'):
+        distri = conta / 16 * 3
+
+    elif (nome == ' Samira'): #Samira mora sozinha e em um apartamento menor:
         v_mensalid = 650
-        distri = conta / 14 * 1
-
-    elif (nome == ' Marilia'):
-        distri = conta / 14 * 1
-
-    elif (nome == ' Lidamara'):
-        v_mensalid = 800
-        distri = conta / 14 * 1
-
+        distri = 0
+        
     saudacoes(conta,distri,leitura,apto,v_mensalid)
 
 def saudacoes(conta,distri,leitura,apto,v_mensalid): #Aqui uma mensagem educada é escrita para o user
@@ -79,33 +53,30 @@ def boleto(v,total,distri,leitura,apto):  #Aqui todos o detalhes do boleto é es
     pular2()
     p.typewrite(f'*LEITURA DA AGUA* - {leitura}',interval=0.05)
     pular2()
-    p.typewrite(f'Alugel do imovel = {v:.2f}R$',interval=0.05)
+    p.typewrite(f'Aluguel do imovel = {v:.2f}R$',interval=0.05)
     pular2()
     p.typewrite(f'Agua = {total:.2f}R$')
     p.hotkey('Shift', 'enter')
     if (apto in lista_aptos):#Os aptos com diversidades são listados aqui.
         p.typewrite(f'Parcial = {distri:.2f}R$', interval=0.05)
         p.hotkey('Shift', 'enter')
-        if apto == '804':
-            p.typewrite('Valor pago externamente: 1200.00R$') #Marilia é um parente, ela paga com servicos domésticos (organização, limpeza, etc.).
-            v -= 1200
-        elif apto == '807':
-            distri = 0
+        if apto == '804' or apto == '802':
+            p.typewrite('Valor pago externamente: 1350.00R$') #Marilia é um parente, ela paga com servicos domésticos (organização, limpeza, etc.).
+            v -= 1350
         elif(False):
             p.typewrite() #Aqui é caso tenha qualquer outra diversidade
         pular2()
         p.typewrite(f'*Valor Total: {v + distri:.2f} R$*', interval=0.05)
-        pular2()
-        p.typewrite('Caso consiga, a melhor forma de recebimento no momento seria em especie; senao, pix tambem sera viavel! ', interval=0.05) #O pagamento pode ser realizado via PIX; caso nao tenha a chave, podemos enviar.
+        pular2()                         #Caso consiga, a melhor forma de recebimento no momento seria em especie; senao, pix tambem sera viavel!
+        p.typewrite('O pagamento pode ser realizado via PIX; caso nao tenha a chave, podemos enviar.', interval=0.05)
         pular2()
         p.typewrite('Nome: PEDRO HENRIQUE', interval=0.05)
         p.hotkey('Shift', 'enter')
         p.typewrite('Instituicao: PagSeguro', interval=0.05)
         pular2()
         p.typewrite(
-            'OBS: Erros podem ocorrer na distribuicao dos valores, caso aconteca, sera corrigido em ate 3min!',
+            'OBS: Erros podem ocorrer na distribuicao dos valores, caso aconteca, sera corrigido em ate 10min.',
             interval=0.05)
-        pular2()
         pular2()
         p.typewrite('_A distribuicao sempre sera feita de forma justa para todos os moradores. Caso tenha qualquer duvida, nos chame por gentileza._',interval=0.05)
         p.sleep(1)
@@ -122,7 +93,7 @@ def boleto(v,total,distri,leitura,apto):  #Aqui todos o detalhes do boleto é es
     pular2()
     p.typewrite(f'*Valor Total: {v + distri:.2f} R$*',interval=0.05)
     pular2()
-    p.typewrite('Caso consiga, a melhor forma de recebimento no momento seria em especie; senao, pix tambem sera viavel! ', interval=0.05)
+    p.typewrite('O pagamento pode ser realizado via PIX; caso nao tenha a chave, podemos enviar.', interval=0.05)
     pular2()
     p.typewrite('Nome: PEDRO HENRIQUE', interval=0.05)
     p.hotkey('Shift', 'enter')
@@ -131,7 +102,6 @@ def boleto(v,total,distri,leitura,apto):  #Aqui todos o detalhes do boleto é es
     p.typewrite(
         'OBS: Erros podem ocorrer na distribuicao dos valores, caso aconteca, sera corrigido em ate 3min!',
         interval=0.05)
-    pular2()
     pular2()
     p.typewrite('_A distribuicao sempre sera feita de forma justa para todos os moradores. Caso tenha qualquer duvida, nos chame por gentileza._', interval=0.05)
     p.sleep(1)
@@ -144,7 +114,7 @@ def boleto(v,total,distri,leitura,apto):  #Aqui todos o detalhes do boleto é es
     p.click(18, 233, duration=0.8)
     return
 
-v_mensalid = 1200.00
+v_mensalid = 1350.00
 
 horario = int(input('Horario de expedição: '))
 bom_dia = horario < 12
@@ -154,10 +124,10 @@ conta = float(input('Valor da conta: '))
 leitura = input('Data da Leitura: ')
 distri = distribuicao(conta)
 
-dicionario = {'801':' Vivian','802':' Flavia','803':' Savia','804':' Marilia','805': ' Nayara','806':' Jeane','807':' Samira','808':' Lidamara'}
+dicionario = {'801':' Vivian','802':' Flavia','803':' Savia','804':' Marilia','805': ' Beatriz','806':' Jeane','807':' Samira'}
 
 #Setor diversidade
-lista=[' Marilia',' Samira',' Lidamara'] #As pessoas que estiverem nesta lista estarão fora do "fluxo comum" do programa, por exemplo: mora sozinho, então o calculo será diferente.
+lista=[' Samira',' Vivian', ' Geane', ' Beatriz', ' Flavia'] #As pessoas que estiverem nesta lista estarão fora do fluxo comum do programa, por exemplo: mora sozinho, então o calculo será diferente.
 
 lista_aptos=['804']#A mesma lógica, porém, contendo mensagens personalizadas para o apartamento que estiver referido.
 #Setor diversidade
@@ -172,4 +142,3 @@ for apto,nome in dicionario.items(): #distribuição para cada inquilino
         ...
     else:
         saudacoes(conta,distri,leitura,apto,v_mensalid)#Fluxo comum do programa.
-
